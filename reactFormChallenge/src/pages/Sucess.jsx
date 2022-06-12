6ff9efd6c47f75520d4708fabf4d107bc65dd40d
WelbertJr <welbertjunior@live.com>
2022-06-12 14:47:48 -0300

    Feat: First commit, adding initial code

---
+++ reactFormChallenge/src/pages/Sucess.jsx
@@ -0,0 +1,17 @@
+function Sucess() {
+  
+    return (
+        <div>
+        <section class="section-page2">
+        <h1 class="h1-page2"><img class="displayed2" src="img/Data1.png"/></h1>
+          <h1 class="h1-page2">Sucess!</h1>
+          
+          <div class="group-sucess">
+            <button id="input-button2" type="submit" value="Go Back!">Go Back!</button>
+          </div>
+      </section>
+      </div>
+    )
+  }
+  
+  export default Sucess
